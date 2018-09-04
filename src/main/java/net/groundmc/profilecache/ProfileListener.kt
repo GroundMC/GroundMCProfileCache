bb7b64f97fe3baacfb7bef9029fdd71e20c4d1fb
Marvin <marv.1997@live.de>
2018-09-04 19:38:17 +0200

    Load player profiles into the cache upon join

--- src/main/java/net/groundmc/profilecache/ProfileListener.kt
+++ src/main/java/net/groundmc/profilecache/ProfileListener.kt
@@ -4,11 +4,22 @@ import com.destroystokyo.paper.event.profile.FillProfileEvent
 import com.destroystokyo.paper.event.profile.LookupProfileEvent
 import com.destroystokyo.paper.event.profile.PreFillProfileEvent
 import com.destroystokyo.paper.event.profile.PreLookupProfileEvent
+import kotlinx.coroutines.experimental.async
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 
 @Suppress("unused")
 object ProfileListener : Listener {
+
+    @EventHandler(priority = EventPriority.LOW)
+    fun eagerCache(event: PlayerJoinEvent) {
+        async {
+            Bukkit.createProfile(event.player.uniqueId).complete(true)
+        }.start()
+    }
 
     @EventHandler
     fun lookupCachedProfile(event: PreLookupProfileEvent) {
