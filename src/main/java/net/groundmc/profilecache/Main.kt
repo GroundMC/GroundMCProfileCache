4e09eaafcd1e5509eb142a1fa1383a9f709f5bef
GiantTree <gianttree@noreply.groundmc.net>
2019-10-30 15:18:12 +0000

    Reduce the maximum amount of concurrent database connections to 2

--- src/main/java/net/groundmc/profilecache/Main.kt
+++ src/main/java/net/groundmc/profilecache/Main.kt
@@ -14,6 +14,7 @@ class Main : JavaPlugin() {
             username = config.getString("database.username", "root")
             password = config.getString("database.password", "")
             transactionIsolation = "TRANSACTION_READ_COMMITTED"
+            maximumPoolSize = 2
     }
 
     override fun onEnable() {
